a3bea38793843b1bec2722974bb782f7caf62cde
Szymon Warszawa <szymonwarszawa@gmail.com>
2020-04-08 17:33:44 +0100

    Implemented funtionality for .clamp() .inRange() and .words() methods.

--- _.js
+++ _.js
@@ -3,6 +3,25 @@ const _ = {
     const lowerClampValue = Math.max(number, lower);
     const clampedValue = Math.min(lowerClampValue, upper);
     return clampedValue;
+  },
+
+  inRange(number, start, end) {
+    if (typeof end === "undefined") {
+      end = start;
+      start = 0;
+    }
+    if (start > end) {
+      let temp = end;
+      end = start;
+      start = temp;
+    }
+    const isInRange = start <= number && number < end;
+    return isInRange;
+  },
+
+  words(string) {
+    const word = string.split(" ");
+    return word;
   }
 };
 
