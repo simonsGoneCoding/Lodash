dee0076017bba8784c9cafdaabcd0347ec1d1884
Szymon Warszawa <szymonwarszawa@gmail.com>
2020-04-14 19:10:47 +0100

    Implemednted functionality for .dropWhile() and .chunk() methods.

--- _.js
+++ _.js
@@ -75,6 +75,23 @@ const _ = {
     const droppedArray = array.slice(n, array.length);
     return droppedArray;
   },
+
+  dropWhile(array, predicate) {
+    const dropNumber = array.findIndex((element, index) => {
+      return !predicate(element, index, array);
+    });
+    const droppedArray = this.drop(array, dropNumber);
+    return droppedArray;
+  },
+
+  chunk(array, size = 1) {
+    let arrayChunks = [];
+    for (let i = 0; i < array.length; i += size) {
+      let arrayChunk = array.slice(i, i + size);
+      arrayChunks.push(arrayChunk);
+    }
+    return arrayChunks;
+  },
 };
 
 // Do not write or modify code below this line.
